ece18b80deed25855cee31c0bdfc2dae587b2d41
kmbrlynn <douglas.kimberly.a@gmail.com>
2015-04-22 18:22:33 -0400

    add termination

--- RegexParsing.cpp
+++ RegexParsing.cpp
@@ -19,8 +19,8 @@ using namespace boost::gregorian;
 
 const int DATE_TIME = 1;
 const int MILLI = 4;
-const int START_BOOT = 6;
-const int END_BOOT = 7;
+const int BOOT_STARTED = 6;
+const int BOOT_ENDED = 7;
 
 int main(int argc, char* argv[]) {
     string line_str, regex_str;
@@ -32,15 +32,15 @@ int main(int argc, char* argv[]) {
     cout << "http://www.boost.org/doc/libs/1_58_0/";
     cout << "boost/regex/v4/error_type.hpp\n" << endl;
 
-    regex_str =                                     //   line           [0]
+    regex_str =                    //   line           [0]
         "([0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2})"        
-                                                    //   date/time      [1]
-                                                    // subgroup ----------- [2]
-        "((: )|"                                    //   empty          [3]
-        ".([0-9]{3}))"                              //   mil            [4]
-                                                    // subgroup ------------[5]
-        "((.*log.c.166.*)|"                         //   boot start     [6]
-        "(.*oejs.Abstr.*))";                        //   boot end       [7]
+                                   //   date/time      [1]
+                                   // subgroup ----------- [2]
+        "((: )|"                   //   empty          [3]
+        ".([0-9]{3}))"             //   mil            [4]
+                                   // subgroup ------------[5]
+        "((.*log.c.166.*)|"        //   boot start     [6]
+        "(.*oejs.Abstr.*))";       //   boot end       [7]
 
     try {
         re = boost::regex(regex_str); 
@@ -60,24 +60,28 @@ int main(int argc, char* argv[]) {
         if (matches[0].matched) {
             string time_str = matches[DATE_TIME] + "." + matches[MILLI];
             cout << time_str;
-//          ptime t(time_from_string(time_str));
+            ptime t(time_from_string(time_str));
+
+            // check for an incomplete boot
+            if (matches[BOOT_STARTED] != "" && (is_booting || EOF))
+                cout << "*** Incomplete boot ***";
+            
 
             // "log.c.166" was found - means boot has started
-            if (matches[START_BOOT] != "" ) {
+            if (matches[BOOT_STARTED] != "" ) {
                 is_booting = true;   
-  //              t1 = t;
-                cout << line_num;
-    //            cout << t1;
+                t1 = t;
+                cout << "=== Device boot ===" << endl;              
+                cout << line_num << "(device-log): " << t1;
                 cout << " Boot started" << endl;
             }
             
             // "oejs.Abstr" was found - means boot has ended
-            if (matches[END_BOOT] != "") { 
+            if (matches[BOOT_ENDED] != "") { 
                 is_booting = false;
-  //              t2 = t;
-                cout << line_num;
-   //             cout << t2;
-                cout << " Boot ended" << endl;
+                t2 = t;
+                cout << line_num << "(device-log): " << t2;
+                cout << " Boot completed" << endl;
             }
 /*
             cout << "Line number is: " << line_num << endl;
