1ba2a0e2c8cc9ec83a9aaafc2c5e96f541431dc7
kmbrlynn <douglas.kimberly.a@gmail.com>
2015-04-23 12:56:19 -0400

    include header and implementation files for ps7b later

--- Regex.hpp
+++ Regex.hpp
@@ -1,1 +1,5 @@
 // Copyright 2015 Kim Douglas
+//
+// This file is not technically needed for ps7a.
+// It is included here for ps7b, which will involve more abstraction.
+// The Makefile is set up accordingly.
