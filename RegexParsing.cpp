a0a225713e6046828631869a242f667640c9e8b1
kmbrlynn <douglas.kimberly.a@gmail.com>
2015-04-23 13:35:07 -0400

    cpplint

--- RegexParsing.cpp
+++ RegexParsing.cpp
@@ -1,24 +1,22 @@
 // Copyright (C) Kim Douglas 2015
-// Based on demo regex code: 
+// Based on demo regex code:
 //      Copyright (C) 2015 Fred Martin, fredm@cs.uml.edu for 91.204 Computing IV
 //      Thu Apr 16 11:08:45 2015
 
 #include <boost/regex.hpp>
-#include "boost/date_time/posix_time/posix_time.hpp"
-#include "boost/date_time/gregorian/gregorian.hpp"
 #include <exception>
 #include <stdexcept>
 #include <fstream>
 #include <iostream>
 #include <string>
 #include "Regex.hpp"
+#include "boost/date_time/posix_time/posix_time.hpp"
+#include "boost/date_time/gregorian/gregorian.hpp"
 
 using std::cin;
 using std::cout;
 using std::endl;
 using std::string;
-using namespace boost::posix_time;
-using namespace boost::gregorian;
 
 const int DATE_TIME = 1;
 const int MILLI = 4;
@@ -26,6 +24,10 @@ const int BOOT_STARTED = 6;
 const int BOOT_ENDED = 7;
 
 int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        throw std::runtime_error("Usage: ./ps7a <filename>");
+    }
+
     std::string in_filename = argv[1];
     std::string out_filename = in_filename + ".rpt";
     std::ifstream inf;
@@ -38,7 +40,7 @@ int main(int argc, char* argv[]) {
 
     std::string regex_str =        //   whole line                  [0]
                                    //   date/time                   [1]
-        "([0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2})"        
+        "([0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2})"
                                    // subgroup ---------------------- [2]
         "((: )|"                   //   empty                       [3]
         ".([0-9]{3}))"             //   mil                         [4]
@@ -47,7 +49,7 @@ int main(int argc, char* argv[]) {
         "(.*oejs.Abstr.*))";       //   boot end                    [7]
 
     try {
-        re = boost::regex(regex_str); 
+        re = boost::regex(regex_str);
     } catch (boost::regex_error& exc) {
         cout << "Regex constructor failed with code " << exc.code() << endl;
         exit(1);
@@ -62,38 +64,39 @@ int main(int argc, char* argv[]) {
     inf.open(in_filename);
     if (inf.is_open()) {
         // holds boot start & end times
-        ptime t1, t2;
+        boost::posix_time::ptime t1, t2;
 
-        while (getline(inf, line_str)) { 
+        while (getline(inf, line_str)) {
             boost::smatch matches;
-            boost::regex_match(line_str, matches, re); 
+            boost::regex_match(line_str, matches, re);
 
             if (matches[0].matched) {
                 // for additional time precision, uncomment the rest of line:
                 string time_str = matches[DATE_TIME]/*+ "." + matches[MILLI]*/;
-                ptime t(time_from_string(time_str));
+                boost::posix_time::ptime
+                              t(boost::posix_time::time_from_string(time_str));
 
                 // check for an incomplete boot
                 if (matches[BOOT_STARTED] != "" && is_booting)
-                    outf << "*** Incomplete boot ***" << endl;            
+                    outf << "*** Incomplete boot ***" << endl;
 
                 // "log.c.166" was found - means boot has started
-                if (matches[BOOT_STARTED] != "" ) {
-                    is_booting = true;   
+                if (matches[BOOT_STARTED] != "") {
+                    is_booting = true;
                     t1 = t;
-                    outf << endl << "=== Device boot ===" << endl;              
+                    outf << endl << "=== Device boot ===" << endl;
                     outf << line_num << "(" << in_filename << "): " << time_str;
                     outf << " Boot Start" << endl;
                 }
-            
+
                 // "oejs.Abstr" was found - means boot has ended
-                if (matches[BOOT_ENDED] != "") { 
+                if (matches[BOOT_ENDED] != "") {
                     is_booting = false;
                     t2 = t;
                     outf << line_num << "(" << in_filename << "): " << time_str;
                     outf << " Boot Completed" << endl;
- 
-                    time_duration td;
+
+                    boost::posix_time::time_duration td;
                     td = t2 - t1;
                     outf << "\tBoot time: " << td.total_milliseconds() << "ms";
                     outf << endl;
@@ -105,7 +108,7 @@ int main(int argc, char* argv[]) {
 
         inf.close();
         outf.close();
-    
+
     } else {
         std::string inf_err = "Unable to open file: " + in_filename;
         throw std::runtime_error(inf_err);
